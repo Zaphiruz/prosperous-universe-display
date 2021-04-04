4c56086dce4080ce396647cba17f06f82c43f271
Zachary Menzer <zac.menzer@gmail.com>
2021-04-03 22:21:32 -0400

    PlanetPage working? Mostly?

--- src/app/pages/planets/planet.js
+++ src/app/pages/planets/planet.js
@@ -4,8 +4,23 @@ import PlanetMat from './planetMat';
 export default ({ planet }) => {
 	return (
 		<div className='bg-gray-400 dark:bg-gray-800 p-2 rounded-md my-2'>
-			<small className='text-gray-500 dark:text-gray-400 mr-2'>{planet.naturalId}</small>
-			<h3 className='text-lg capitalize inline-block'>{planet.naturalId}</h3>
+			<span>
+				<small className='text-gray-500 dark:text-gray-400 mr-2'>{planet.naturalId} </small>
+				<h3 className='text-lg capitalize inline-block'>{planet.naturalId} </h3>
+			</span>
+			<span>
+				<small className='text-gray-500 dark:text-gray-400 mr-2'>Fertility</small>
+				<h3 className='text-lg capitalize inline-block'>{planet.data.fertility}</h3>
+			</span>
+			<h3 className='text-lg capitalize inline-block'>  Tiers  </h3>
+			<small className='text-gray-500 dark:text-gray-400 mr-2'>Overall</small>
+			<h3 className='text-lg capitalize inline-block'>{planet.tier.planetTier}</h3>
+			<small className='text-gray-500 dark:text-gray-400 mr-2'>Gravity</small>
+			<h3 className='text-lg capitalize inline-block'>{(planet.tier.gravity === -1) ? "Low" : ((planet.tier.gravity === 1) ? "High": "Normal")}</h3>
+			<small className='text-gray-500 dark:text-gray-400 mr-2'>Pressure</small>
+			<h3 className='text-lg capitalize inline-block'>{(planet.tier.pressure === -1) ? "Low" : ((planet.tier.pressure === 1) ? "High" : "Normal")}</h3>
+			<small className='text-gray-500 dark:text-gray-400 mr-2'>Temperature</small>
+			<h3 className='text-lg capitalize inline-block'>{(planet.tier.temperature === -1) ? "Low" : ((planet.tier.temperature === 1) ? "High" : "Normal")}</h3>
 
 			<div>
 				{planet.data.resources.map((resource) => (
--- src/app/pages/planets/planets.js
+++ src/app/pages/planets/planets.js
@@ -31,6 +31,7 @@ const PlanetQuery = {
 		mass: true,
 		massEarth: true,
 		orbitIndex: true,
+		plots: true,
 		pressure: true,
 		radiation: true,
 		radius: true,
@@ -45,7 +46,8 @@ const PlanetQuery = {
 		},
 		sunlight: true,
 		surface: true,
-		temperature: true
+		temperature: true,
+		takenPlots: true
 	},
 	governor: true,
 	id: true,
@@ -55,7 +57,13 @@ const PlanetQuery = {
 	namingDate: true,
 	naturalId: true,
 	planetId: true,
-	populationId: true
+	populationId: true,
+	tier: {
+		gravity: true,
+		pressure: true,
+		temperature: true,
+		planetTier: true
+    }
 };
 
 export default () => {
@@ -63,6 +71,10 @@ export default () => {
 
 	let [planets, setPlanets] = useState([]);
 	let [company, setCompany] = useState({});
+	let [filterMaterial, setFilterMaterial] = useState('');
+	let [planetResults, setPlanetResults] = useState([]);
+	let [filterPlanetTier, setFilterPlanetTier] = useState('');
+	let [filterFertility, setFertility] = useState(-1);
 
 	const fetchPlanets = async () => {
 		console.log("About to query");
@@ -74,9 +86,54 @@ export default () => {
 
 	useEffect(async () => {
 		let planets = await fetchPlanets();
+		setPlanetResults(planets);
 	}, []);
 
 	const resourcesUnique = _.uniq(_.flatMap(planets.map((planet) => planet.data.resources.map((resource) => resource.material.ticker)))).sort(function (a, b) { return a.localeCompare(b) });
+
+	const handleMaterialSearchChange = event => {
+		setFilterMaterial(event.target.value);
+	};
+
+	const handleTierSearchChange = event => {
+		setFilterPlanetTier(event.target.value);
+	};
+
+	const handleFertilitySearchChange = event => {
+		setFertility(parseFloat(event.target.value));
+	};
+
+	React.useEffect(() => {
+		let filteredPlanetsSet = planets;
+
+		// Material Filter
+		if (filterMaterial) {
+			filteredPlanetsSet = filteredPlanetsSet.filter(planet =>
+				planet.data.resources.some(mat =>
+					mat.material.ticker === filterMaterial)
+			).sort(function (a, b) {
+				return b.data.resources.find(resource => resource.material.ticker === filterMaterial).factor - a.data.resources.find(resource => resource.material.ticker === filterMaterial).factor
+			});
+		}
+
+		// filter by fertility
+		if (filterFertility != -1) {
+			filteredPlanetsSet = filteredPlanetsSet.filter(planet =>
+				planet.data.fertility >= filterFertility);
+        }
+
+		// filter by planet tier
+		if (filterPlanetTier != 0) {
+			filteredPlanetsSet = filteredPlanetsSet.filter(planet =>
+				planet.tier.planetTier >= filterPlanetTier);
+        }
+
+		setPlanetResults(filteredPlanetsSet);
+
+	}, [filterMaterial, filterPlanetTier, filterFertility]);
+
+
+	// Show what would take to build on the planet
 
 	return (
 		<div className='consuption-report container mx-auto p-3'>
@@ -85,17 +142,31 @@ export default () => {
 			<div>
 				<label>
 					<strong> Filter:</strong>
-					<select className="text-black">
+					<select className="text-black" onChange={ handleMaterialSearchChange }>
 						<option key="All" value="">All</option>
 						{resourcesUnique.map(mat => (
 							<option key={mat} value={mat}> {mat} </option>
 						))}
 					</select>
+
+					<strong> Planet Tier: </strong>
+					<select className="text-black" onChange={handleTierSearchChange}>
+						<option key="all" value="">All</option>
+						<option key="0" value="0">0</option>
+						<option key="1" value="1">1</option>
+						<option key="2" value="2">2</option>
+						<option key="3" value="3">3</option>
+					</select>
+					
+					<strong> Fertility </strong>
+					<select className="text-black" onChange={handleFertilitySearchChange }>
+						<option key="tmp" value="tmp">tmp</option>
+					</select>
 				</label>
 			</div>
 
 			<div className=''>
-				{planets.map(planet => (
+				{planetResults.map(planet => (
 					<PlanetVisual planet={planet} key={ planet.naturalId }/>	
 				))}
 			</div>
