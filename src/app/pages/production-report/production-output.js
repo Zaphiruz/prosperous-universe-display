f124ef55e0990927fa4ac4b4fa818bf04fb62e7e
Zachary Menzer <zac.menzer@gmail.com>
2021-04-10 00:17:27 -0400

    Further work on the production line page

---
+++ src/app/pages/production-report/production-output.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { startCase } from 'lodash';
+
+const returnOrders = (orders) => {
+	let final = orders.map((order) => {
+		if (order.completed > 0) {
+			return "";
+		};
+		return order.outputs.map((output) => {
+			return output.material.ticker;
+		});
+	})
+		.filter(Boolean)
+		.join(',');
+
+	return final;
+};
+
+const returnConsumption = (orders) => {
+	let final = orders.map((order) => {
+		if (order.completed > 0) {
+			return "";
+		};
+		return order.inputs.map((input) => {
+			return input.material.ticker;
+		});
+	})
+		.filter(Boolean)
+		.join(',');
+
+return final;
+};
+
+export default ({ orders }) => {
+
+	return (
+		<div className={`p-2 rounded-md bg-gray-400 dark:bg-gray-800`}>
+			<h3 className='text-lg capitalize inline-block'>Making {returnOrders(orders)}</h3> {"\n"}
+			<h3 className='text-lg capitalize inline-block'>Burning {returnConsumption(orders)}</h3>
+		</div>
+	)
+};
