bce81685a2b2c61fae1273683f866e4f937285d1
Zachary Menzer <zac.menzer@gmail.com>
2021-05-25 23:49:24 -0400

    Base planner test

--- src/app/pages/base-planner/base-planner.js
+++ src/app/pages/base-planner/base-planner.js
@@ -6,6 +6,7 @@ import config from 'ROOT/config';
 import './base-planner.less';
 
 import Loading from 'COMPONENTS/loading';
+import Button from 'COMPONENTS/button';
 
 const PlanetQuery = {
 	_id: true,
@@ -257,26 +258,186 @@ const ProductionQuery = {
 export default () => {
 	console.log("Loading page...");
 
-	let [planetResults, setPlanetResults] = useState([]);
-	let [distanceTarget, setDistanceTarget] = useState('Moria');
-
-	let [paginationPage, setPaginationPage] = useState(1);
-	let [paginationMax, setPaginationMax] = useState(-1);
-	let [pageLoadState, setPageLoadState] = useState(0);
-
-	let materialFilterRef = useRef('');
+	let [error, setError] = useState('');
+	let [itemList, setItemList] = useState([]);
 
 	useEffect(async () => {
 		//let planets = await fetchPlanets();
 		//setPlanetResults(planets);
-		paginateQuery(1, 50);
 	}, []);
 
+	const addItem = async (e) => {
+		console.log("Would have done something");
+	}
+
+	const removeItem = async (i, e) => {
+		console.log("Would have removed something");
+    }
+
+	const resetList = async (i, e) => {
+		console.log("Would have reset");
+	}
 
 	return (
-		<div className='base-planner container mx-auto p-3'>
-			<div>
-				<h3> Base Planner temp page</h3>
+		<div className='base-planning container mx-auto p-3'>
+			<h1 className='text-xl capitalize inline-block'>Base Planner</h1>
+
+			<div className='lg:flex'>
+				<div className='w-4/5 lg:mr-4'>
+					<form onSubmit={addItem}>
+						<table>
+							<thead>
+								<tr>
+									<td rowSpan="1"></td>
+									<td rowSpan="1"></td>
+									<td rowSpan="1"></td>
+									<th colSpan="4">BFabs</th>
+									<th colSpan="4">LFabs</th>
+									<th colSpan="4">RFabs</th>
+									<th colSpan="4">AFabs</th>
+									<th colSpan="4">Planet</th>
+								</tr>
+								<tr>
+									<th>Building</th>
+									<th>Number</th>
+									<th>Area</th>
+									<th>BBH</th>
+									<th>BDE</th>
+									<th>BSE</th>
+									<th>BTA</th>
+									<th>LBH</th>
+									<th>LDE</th>
+									<th>LSE</th>
+									<th>LTA</th>
+									<th>RBH</th>
+									<th>RDE</th>
+									<th>RSE</th>
+									<th>RTA</th>
+									<th>ABH</th>
+									<th>ADE</th>
+									<th>ASE</th>
+									<th>ATA</th>
+									<th>Ground</th>
+									<th>Gravity</th>
+									<th>Temp</th>
+									<th>Pressure</th>
+									<th>&nbsp;</th>
+								</tr>
+							</thead>
+
+							<tbody>
+								{itemList.map((listItem, i) => (
+									<tr key={listItem.ticker}>
+										<td>building</td>
+										<td>num</td>
+										<td>area</td>
+										<td>bfab</td>
+										<td>bfab</td>
+										<td>bfab</td>
+										<td>bfab</td>
+										<td>lfab</td>
+										<td>lfab</td>
+										<td>lfab</td>
+										<td>lfab</td>
+										<td>rfab</td>
+										<td>rfab</td>
+										<td>rfab</td>
+										<td>rfab</td>
+										<td>afab</td>
+										<td>afab</td>
+										<td>afab</td>
+										<td>afab</td>
+										<td>planet</td>
+										<td>planet</td>
+										<td>planet</td>
+										<td>planet</td>
+										<td>
+											<Button type='button'
+												className='bg-red-500 dark:bg-red-700 flex justify-center items-center'
+												size='sm'
+												onClick={(e) => removeItem(i, e)}
+											>
+												<span className="material-icons text-sm">clear</span>
+											</Button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+
+							<tfoot className='divide-y divide-blue-700 divide-opacity-30'>
+								<tr>
+									<td>
+										<input type='text'
+											tabIndex="1"
+											id='building'
+											name='building'
+											className='mr-2'
+											required="required"
+											pattern='[a-zA-Z0-9]{1,3}'
+											placeholder='i.e. HB1'
+											title="Ticker should only contain 1-3 charactors. i.e. RAT, DW, H"
+										/>
+									</td>
+									<td>
+										<input type='text'
+											tabIndex="2"
+											id='count'
+											name='count'
+											className='mr-2'
+											required="required"
+											pattern='\d+'
+											placeholder='i.e. 2'
+											title="Count should only be positive numbers i.e. 1, 12, 123, 1234"
+										/>
+									</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+									<td>0</td>
+								</tr>
+
+								<tr>
+									<td>
+										<Button type='button'
+											className='bg-red-500 dark:bg-red-700'
+											onClick={resetList}
+										>
+											Reset
+										</Button>
+									</td>
+								</tr>
+							</tfoot>
+
+						</table>
+					</form>
+					<h3>Add totals</h3>
+					<h3>Add planet selection</h3>
+					<h3>Add save/load</h3>
+					<h3>Add costs - Corp pricing? CX?</h3>
+					<h3>Add section sharing what the planet produces each day (fertility, mats, etc)</h3>
+					<h3>Add section on how far a planet is from CXs</h3>
+					<h3>Add section on how many workers you'll need for each type</h3>
+					<h3>Add area usage</h3>
+				</div>
+
+				
 			</div>
 		</div>
 	);
